ff36b367846887b685cdbc4f9b15f6a45f9236ed
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-09-24 11:55:50 +0900

    add start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 VERSION=0.1.0b
 echo "plathome application ver${VERSION}"
 
@@ -16,7 +16,7 @@ else
 fi
 
 echo "checking git folder..."
-cd plathome-frontend
+cd plathome-frontend || { echo "move plathome-frontend failed" && exit 1; }
 STATUS=`git status`
 
 FATALGITSTATUS="fatal: not a git repository (or any of the parent directories): .git"
@@ -24,6 +24,8 @@ if [ "$STATUS" == "$FATALGITSTATUS" ]; then
   echo "error: git status failed"
   exit 1
 fi
+
+git pull
 
 cd ..
 ## backend-checking
@@ -40,16 +42,41 @@ else
 fi
 
 echo "checking git status."
-cd plathome-backend
+cd plathome-backend || { echo "move plathome-backend failed" && exit 1; }
 STATUS=`git status`
 
 
-FATALGITSTATUS="fatal: not a git repository (or any of the parent directories): .git"
 if [ "$STATUS" == "$FATALGITSTATUS" ]; then
   echo "error: git status failed"
   exit 1
 fi
+git pull
 
 cd ..
 
-# WIP: Docker
+
+ENV="BASEURL=http://localhost:8080/"
+cd plathome-frontend || { echo "move plathome-frontend failed" && exit 1; }
+# SETUP .env for plathome-frontend
+if [ -e ./.env ]; then
+  CAT_ENV=`cat .env`
+  if [ "$CAT_ENV" == "$ENV" ]; then
+  echo "env found"
+
+  else
+    echo ".env corrupted. deleting .env ..."
+    rm .env
+    echo $ENV > .env
+  fi
+else
+  echo "creating .env ..."
+  echo $ENV > .env
+fi
+cd ..
+
+
+
+# Starting Docker Compose...
+echo "docker-compose start"
+docker-compose -f dev-docker-compose.yml build
+docker-compose  -f dev-docker-compose.yml up
